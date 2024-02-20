5e751b823166747964639fd068bfb6e0ca43549a
Gavin_Erh <Gavin_Erh@singaporeair.com.sg>
2024-02-20 10:06:39 +0800

    added obj generation code for frontend request

--- src/test/java/MARMSUI/testCaseCreation/CreateObjectForFrontend.java
+++ src/test/java/MARMSUI/testCaseCreation/CreateObjectForFrontend.java
@@ -1,7 +1,9 @@
 package MARMSUI.testCaseCreation;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CreateObjectForFrontend {
     public static void main(String[] args) {
@@ -867,11 +869,113 @@ public class CreateObjectForFrontend {
         String[] arr = request.split("\n");
         List<String> fields = new ArrayList<>();
         List<String> types = new ArrayList<>();
-        extractFieldAndType(fields, types);
+        printInitializationCode(arr,fields, types);
     }
 
-    private static void extractFieldAndType(List<String> fields, List<String> types) {
+    private static void printInitializationCode(String[] requestArr, List<String> fields, List<String> types) {
+        int count = 0;
+        String toPrint = "%s %s = new %s();";
+        Set<String> varNames = new HashSet<>();
+        for(String row : requestArr) {
+            String type = extractType(row);
+            String currentFieldName = extractCurrentFieldName(row);
+//            if(row.contains("guardianInfo")) {
+//                System.out.println("");
+//            }
+            if(type.startsWith("com.sg.sq.marmsui")) {
+                if(type.contains("[]")) {
+                    int varCountForObj = count;
+                    int varCountForArr = count+1;
+                    type = extractTypeWithoutArrayDeclaration(type);
+//                    String varName = extractVariableNameFromClassName(type);
+                    if(!varNames.contains(currentFieldName)){
+                        System.out.println(String.format(toPrint, type, currentFieldName,type));
+                        varNames.add(currentFieldName);
+                    }
+                    System.out.println(String.format("%s[] %s%s = new %s[1];",type,"var", varCountForArr,type));
 
+                    System.out.println(String.format("%s%d[0] = %s;", "var", varCountForArr,currentFieldName));
+                    System.out.println(String.format("%s.%s(%s%d);",extractPreviousFieldName(row),generateSetterMethod(currentFieldName),"var",varCountForArr));
+                } else if (type.startsWith("java.util.List<")) {
+                    throw new RuntimeException("Not implemented yet for list");
+                } else if (type.startsWith("java.util.Map<")){
+                    throw new RuntimeException("Not implemented yet for maps");
+                } else if (type.startsWith("com.sg.sq.marmsui")) {
+                    if(!varNames.contains(currentFieldName)) {
+                        System.out.println(String.format("%s %s = new %s();", type,currentFieldName,type));
+                        varNames.add(currentFieldName);
+                    }
+                    System.out.println(String.format("%s.%s(%s);",extractPreviousFieldName(row),generateSetterMethod(currentFieldName),currentFieldName));
+                }
+            }
+            count++;
+        }
+    }
+
+    private static String extractVariableNameFromClassName(String type) {
+        int start = 0;
+        int end = type.length();
+        int count = 0;
+        for(char c : type.toCharArray()) {
+            if(c == '.') {
+                start = count;
+            }
+            count++;
+        }
+        start++;
+        String var = type.substring(start,end);
+        return var.substring(0,1).toLowerCase() + var.substring(1);
+    }
+
+    private static String generateSetterMethod(String fieldName) {
+        return "set" + fieldName.substring(0,1).toUpperCase() + fieldName.substring(1);
+    }
+
+    private static String extractPreviousFieldName(String row) {
+        int prevStart = 0;
+        int endStart = 0;
+        int count = row.indexOf("(");
+        String fieldsOnly = row.substring(0,count);
+        count = 0;
+        for(char c : fieldsOnly.toCharArray()) {
+            if(c == '-') {
+                if(prevStart != endStart) {
+                    prevStart = endStart;
+                }
+                endStart = count;
+            }
+            count++;
+        }
+        if(endStart == 0 || prevStart == endStart) {
+            return "oriVar";
+        }
+        prevStart++;
+        return fieldsOnly.substring(prevStart,endStart);
+    }
+
+    private static String extractCurrentFieldName(String row) {
+        int start = 0;
+        int end = row.indexOf("(");
+        int count = 0;
+        for(char c : row.toCharArray()) {
+            if(c == '-') {
+                start = count;
+            }
+            count++;
+        }
+        start++;
+        return row.substring(start,end);
+    }
+
+    private static String extractType(String row) {
+        int start = row.indexOf("(") + 1;
+        int end = row.indexOf(")", start);
+        return row.substring(start,end);
+    }
+
+    private static String extractTypeWithoutArrayDeclaration(String type) {
+        int end = type.indexOf("[");
+        return type.substring(0,end);
     }
 
     // numeric types, int, long, java.lang.Long
